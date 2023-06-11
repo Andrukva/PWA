cd1764e2ea98fe87219c7134f158a95cebb66288
Andrukva <76404667+Andrukva@users.noreply.github.com>
2023-06-11 12:21:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const staticCasheName = "app-v1";
+const staticCacheName = "app-v1";
 
 const assetUrls = [
-    'index.html',
-    '/js/main.js',
-    '/css/style.css',
-    '/img/delete.png',
-    '/img/done.png',
-    '/img/empty-folder32.png',
-    '/img/empty-folder64.png',
-    '/img/fact_check.png',
-    '/img/todo24.png',
-    '/img/todo64.png',
-    '/img/todo128.png',
-    '/img/t256.png',
-    '/img/todo512.png',
-]
+  'index.html',
+  '/js/main.js',
+  '/css/style.css',
+  '/img/delete.png',
+  '/img/done.png',
+  '/img/empty-folder32.png',
+  '/img/empty-folder64.png',
+  '/img/fact_check.png',
+  '/img/todo24.png',
+  '/img/todo64.png',
+  '/img/todo128.png',
+  '/img/t256.png',
+  '/img/todo512.png',
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(cacheAssets());
